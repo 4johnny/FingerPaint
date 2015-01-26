341d0c479eba57336438919802c5d5e0a8c3a9e3
4johnny <4johnny@users.noreply.github.com>
2015-01-25 18:50:46 -0800

    Initial Commit

---
+++ FingerPaint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FingerPaint
+//
+//  Created by Johnny on 2015-01-25.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
